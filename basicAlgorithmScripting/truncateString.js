04d896d51006ef6f0498b8a2a83941e3ca87c01c
aaronsato <2asato@gmail.com>
2020-09-01 15:15:07 -0700

    solved truncateString

---
+++ basicAlgorithmScripting/truncateString.js
@@ -0,0 +1,14 @@
+// Truncate a string (first argument) 
+// if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+    if (str.length <= num) {
+      return str
+    }
+    let truncatedStr = str.slice(0, num)  + '...';
+    console.log(truncatedStr)
+    return truncatedStr;
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
